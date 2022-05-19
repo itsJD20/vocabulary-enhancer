d480cf5b10b4798cf955b303a8e3ac9508e316fd
joyatee-datta-2020 <joyetaratri123@gmail.com>
2022-05-20 01:53:24 +0600

    Update OMW

--- get_knowledge.py
+++ get_knowledge.py
@@ -2,7 +2,7 @@ import nltk
 nltk.download('wordnet')
 nltk.download('stopwords')
 nltk.download('punkt')
-
+nltk.download('omw-1.4')
 from keytotext import pipeline
 from rake_new2 import Rake
 from nltk.corpus import wordnet
